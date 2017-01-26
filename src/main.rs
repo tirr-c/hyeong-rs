fbe7e6aec1fa634242e8a37c450a287f786cafd4
VBChunguk <chwo9843@gmail.com>
2017-01-26 22:45:19 +0900

    Add basic structures

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,29 @@
+struct Instruction {
+    op: Operation,
+    dots: usize,
+    hearts: HeartTree,
+}
+
+struct Operation {
+    op_type: OperationType,
+    hangul_count: usize,
+}
+
+enum OperationType {
+    Push,           // 형
+    Add,            // 항
+    Multiply,       // 핫
+    Negate,         // 흣
+    Reciprocate,    // 흡
+    Duplicate,      // 흑
+}
+
+enum HeartTree {
+    Heart(usize),
+    Return,
+    LessThan(Box<HeartTree>, Box<HeartTree>),
+    Equals(Box<HeartTree>, Box<HeartTree>),
+}
+
 fn main() {
 }
