7b65cb0af31e1069644cae0c0c54b1246e58a9ca
Wonwoo Choi <chwo9843@gmail.com>
2022-02-05 14:47:34 +0900

    Upgrade clap to 3.x

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,63 @@
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
+use std::path::PathBuf;
 
-use rshyeong::{HyeongReadStack, HyeongWriteStack, Parser, Processor, StackManager};
+use clap::Parser;
+use rshyeong::{HyeongReadStack, HyeongWriteStack, Processor, StackManager};
+
+#[derive(Debug, Parser)]
+#[clap(version, about)]
+struct Options {
+    #[clap(short, long, default_value = "-")]
+    /// Input file, stdin by default
+    input: PathBuf,
+    /// Output file, stdout by default
+    #[clap(short, long, default_value = "-")]
+    output: PathBuf,
+    /// Input source code
+    source: PathBuf,
+}
 
 fn main() {
-    let yaml = clap::load_yaml!("cli.yml");
-    let matches = clap::App::from_yaml(yaml).get_matches();
+    let Options {
+        input,
+        output,
+        source,
+    } = Options::parse();
 
-    let source_file = matches.value_of("SOURCE").unwrap();
-    let mut source = match File::open(source_file) {
+    let mut source = match File::open(source) {
         Ok(f) => f,
         Err(e) => {
-            eprintln!("Cannot open source file {}: {}", source_file, e);
+            eprintln!("Cannot open source file: {}", e);
             std::process::exit(2);
         }
     };
     let mut source_string = String::new();
     if let Err(e) = source.read_to_string(&mut source_string) {
-        eprintln!("Cannot read source file {}: {}", source_file, e);
+        eprintln!("Cannot read source file: {}", e);
         std::process::exit(2);
     }
 
-    let input_file = matches.value_of("input").unwrap_or("-");
-    let stdin: HyeongReadStack<Box<dyn Read>> = if input_file == "-" {
+    let stdin: HyeongReadStack<Box<dyn Read>> = if input.as_os_str() == "-" {
         HyeongReadStack::new(Box::new(std::io::stdin()))
     } else {
-        let file = match File::open(input_file) {
+        let file = match File::open(input) {
             Ok(f) => f,
             Err(e) => {
-                eprintln!("Cannot open input file {}: {}", input_file, e);
+                eprintln!("Cannot open input file: {}", e);
                 std::process::exit(2);
             }
         };
         HyeongReadStack::new(Box::new(file))
     };
 
-    let output_file = matches.value_of("output").unwrap_or("-");
-    let stdout: HyeongWriteStack<Box<dyn Write>> = if output_file == "-" {
+    let stdout: HyeongWriteStack<Box<dyn Write>> = if output.as_os_str() == "-" {
         HyeongWriteStack::new(Box::new(std::io::stdout()))
     } else {
-        let file = match File::create(output_file) {
+        let file = match File::create(output) {
             Ok(f) => f,
             Err(e) => {
-                eprintln!("Cannot open output file {}: {}", output_file, e);
+                eprintln!("Cannot open output file: {}", e);
                 std::process::exit(2);
             }
         };
@@ -52,7 +67,7 @@ fn main() {
     let stderr = HyeongWriteStack::new(std::io::stderr());
 
     let stacks = StackManager::from_stacks(stdin, stdout, stderr);
-    let parser = Parser::new(&source_string);
+    let parser = rshyeong::Parser::new(&source_string);
     let processor = Processor::with_stack_manager(parser, stacks);
 
     let (exit_code, err) = processor.run();
